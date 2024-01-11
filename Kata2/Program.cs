b68f57d1f800a5ecd47646a65033ca3764ffe337
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-11 16:16:42 +0100

    Add project files.

---
+++ Kata2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Kata2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Here's the total sum converted into binary:");
+            Console.WriteLine(AddBinary(a, b));
+        }
+        public static string AddBinary(int a, int b)
+        {
+            return Convert.ToString((a + b), 2);
+        }
+    }
+}
